e24712c02aa92882883335ae11093a1c244fac40
Zachary Wenta <zwenta@carthage.edu>
2014-07-17 15:32:28 -0500

    more work on form fields

--- djspace/registration/form_fields.py
+++ djspace/registration/form_fields.py
@@ -32,64 +32,109 @@ To do pile
     """
     If undergrad
     """
-    Highschool name
-    Highschool street
-    Highschool city
-    Highschool state
-    Highschool ZIP
-    WSGC college enrolled or applied to
-    WSGC college enrolled or applied to student number
-    WSGC advisor salutation
-    WSGC advisor first name
-    WSGC advisor middle name
-    WSGC advisor last name
-    WSGC advisor title/department
-    WSGC advisor email
-    WSGC advisor confirm email
-    WSGC advisor phone
-    Degree seeking
-    Ultimate degree seeking
-    Major
-    Major 2
-    Minor
-    SAT Verbal
-    SAT Math
-    SAT Total
-    ACT English
-    ACT Math
-    ACT Reading
-    ACT Science reas.
-    ACT composite
-    curret cumulative gpa
-    Major GPA
-    Scale
-    Credits
-    Year in school (as of next fall)
-    Expected date of graduation (mm/dd/yyyy)
-    Area of Undergraduate Concentration in a Space, Aerospace, or Space-Related Field
+        Highschool name
+        Highschool street
+        Highschool city
+        Highschool state
+        Highschool ZIP
+        WSGC college enrolled or applied to
+        WSGC college enrolled or applied to student number
+        WSGC advisor salutation
+        WSGC advisor first name
+        WSGC advisor middle name
+        WSGC advisor last name
+        WSGC advisor title/department
+        WSGC advisor email
+        WSGC advisor confirm email
+        WSGC advisor phone
+        Degree seeking
+        Ultimate degree seeking
+        Major
+        Major 2
+        Minor
+        SAT Verbal
+        SAT Math
+        SAT Total
+        ACT English
+        ACT Math
+        ACT Reading
+        ACT Science reas.
+        ACT composite
+        curret cumulative gpa
+        Major GPA
+        Scale
+        Credits
+        Year in school (as of next fall)
+        Expected date of graduation (mm/dd/yyyy)
+        Area of Undergraduate Concentration in a Space, Aerospace, or Space-Related Field
         
         
         
     """
     If graduate
     """
-    Highschool graduated from name
-    Highschool graduated from street
-    Highschool graduated from city
-    Highschool graduated from state
-    Highschool graduated from ZIP
-    WSGC Institution Enrolled in or Applied to for Fall of next academic year
-    WSGC Institution Enrolled in or Applied to for Fall of next academic year student number    
-    WSGC advisor salutation
-    WSGC advisor first name
-    WSGC advisor middle name
-    WSGC advisor last name
-    WSGC advisor title/department
-    WSGC advisor email
-    WSGC advisor confirm email
-    WSGC advisor phone
-    .....
+        Highschool graduated from name
+        Highschool graduated from street
+        Highschool graduated from city
+        Highschool graduated from state
+        Highschool graduated from ZIP
+        WSGC Institution Enrolled in or Applied to for Fall of next academic year
+        WSGC Institution Enrolled in or Applied to for Fall of next academic year student number    
+        WSGC advisor salutation
+        WSGC advisor first name
+        WSGC advisor middle name
+        WSGC advisor last name
+        WSGC advisor title/department
+        WSGC advisor email
+        WSGC advisor confirm email
+        WSGC advisor phone
+        University name
+        University street
+        University city
+        University state
+        University ZIP
+        Undergraduate degree (associate or bachelor)
+        Undergraduate major
+        Undergraduate major2
+        Undergraduate minor
+        Undergraduate gpa
+        Undergraduate scale
+        Undergraduate credits
+        Undergraduate honors
+        Graduation date mm/dd/yyyy
+        GRE verbal score   <<< Graduate school VVV
+        GRE quantitative score
+        GRE analytic score
+        Other test name
+        Other score
+        Current degree seeking (master's, doctorate or other)
+        Ultimate degree seeking
+        Major
+        Emphasis
+        Current GPA
+        Major GPA
+        Scale
+        Credits
+        Year in school as of next fall
+        Expected date of graduation mm/dd/yyyy
     
+    
+    
+    """
+    if professional
+    """
+        Employer (select)
+        Employer name
+        Employer street
+        Employer city
+        Employer state
+        Employer ZIP
+        
+        
+        
+    """
+    If professor
+    """
 
 """
 Personal
