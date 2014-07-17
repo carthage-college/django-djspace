407c96e4a6040ca4b1b93885a738f019375d5eda
Zachary Wenta <zwenta@carthage.edu>
2014-07-17 15:11:58 -0500

    initial work on what form fields exist

---
+++ djspace/registration/form_fields.py
@@ -0,0 +1,97 @@
+"""
+Catagorize all of these fields
+"""
+
+"""
+To do pile
+"""
+    Tribal or AISES rocket competition (boolean)
+    US citizen (boolean)
+    First name
+    Middle name
+    Last name
+    Maiden name
+    Additional name
+    Title / department
+    Web page
+    Street
+    City
+    State
+    ZIP
+    Phone number
+    Primary interest (select)
+    Primary interest (other)
+    Secondary interest (select)
+    Secondary interest (other)
+    Birthdate
+    Gender
+    Disability (boolean)
+    Race (select)
+    Tribe
+    
+    """
+    If undergrad
+    """
+    Highschool name
+    Highschool street
+    Highschool city
+    Highschool state
+    Highschool ZIP
+    WSGC college enrolled or applied to
+    WSGC college enrolled or applied to student number
+    WSGC advisor salutation
+    WSGC advisor first name
+    WSGC advisor middle name
+    WSGC advisor last name
+    WSGC advisor title/department
+    WSGC advisor email
+    WSGC advisor confirm email
+    WSGC advisor phone
+    Degree seeking
+    Ultimate degree seeking
+    Major
+    Major 2
+    Minor
+    SAT Verbal
+    SAT Math
+    SAT Total
+    ACT English
+    ACT Math
+    ACT Reading
+    ACT Science reas.
+    ACT composite
+    curret cumulative gpa
+    Major GPA
+    Scale
+    Credits
+    Year in school (as of next fall)
+    Expected date of graduation (mm/dd/yyyy)
+    Area of Undergraduate Concentration in a Space, Aerospace, or Space-Related Field
+        
+        
+        
+    """
+    If graduate
+    """
+    Highschool graduated from name
+    Highschool graduated from street
+    Highschool graduated from city
+    Highschool graduated from state
+    Highschool graduated from ZIP
+    WSGC Institution Enrolled in or Applied to for Fall of next academic year
+    WSGC Institution Enrolled in or Applied to for Fall of next academic year student number    
+    WSGC advisor salutation
+    WSGC advisor first name
+    WSGC advisor middle name
+    WSGC advisor last name
+    WSGC advisor title/department
+    WSGC advisor email
+    WSGC advisor confirm email
+    WSGC advisor phone
+    .....
+    
+
+"""
+Personal
+"""
+
