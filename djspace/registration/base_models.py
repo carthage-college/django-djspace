f57862dfa8d8ac20bcc5714727dd55acb80bfeff
reZach <reZach@users.noreply.github.com>
2014-07-18 09:11:31 -0500

    Update base_models.py
    added basepersonalinformation class

--- djspace/registration/base_models.py
+++ djspace/registration/base_models.py
@@ -4,6 +4,42 @@ from django.db import models, connection
 from django.contrib.auth.models import User  
 
 
+class BasePersonalInformation(models.Model):
+    
+    GENDER = (
+     ('male','Male'),('female','Female')   
+    )
+    
+    first = models.CharField(
+        "First name",
+        max_length=20
+    )
+    middle = models.CharField(
+        "Middle name",
+        max_length=20
+    )
+    last = models.CharField(
+        "Last name",
+        max_length=20
+    )
+    email = models.EmailField(
+        "Email address" 
+    )
+    birth = models.DateField(
+        "Birthdate",
+        auto_now=False
+    )
+    phone = models.CharField(
+        "Phone number",
+        max_length=16
+    )
+    gender = models.CharField(
+        "Gender",
+        max_length=8,
+        choices=GENDER
+    )
+    
+
 class BaseLocationInformation(models.Model):
     
     name = models.CharField(
