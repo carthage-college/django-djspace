99574f69c814766dd758fb4559be6b7d41a8e34e
Zachary Wenta <zwenta@carthage.edu>
2014-07-22 15:31:59 -0500

    removed unnecessary imports

--- djspace/registration/form_models/professional.py
+++ djspace/registration/form_models/professional.py
@@ -2,7 +2,6 @@
 from django.conf import settings
 from django.db import models, connection
 from django.contrib.auth.models import User
-from djspace.registration.base_models import *
 
 from djtools.fields import BINARY_CHOICES, SALUTATION_TITLES, STATE_CHOICES
 from djtools.fields import GENDER_CHOICES
