d4237e9ca22c898a997e7890284afaff91ad73fc
reZach <reZach@users.noreply.github.com>
2014-07-23 13:17:40 -0500

    Delete models.py

--- djspace/registration/models.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.conf import settings
-from django.db import models, connection
-from django.contrib.auth.models import User
-
-from djtools.fields import BINARY_CHOICES, SALUTATION_TITLES, STATE_CHOICES
-from djtools.fields import GENDER_CHOICES
-
