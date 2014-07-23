9368a3683ecee3eb28de8fd43fec6d60a999afc4
Zachary Wenta <zwenta@carthage.edu>
2014-07-23 13:27:30 -0500

    import fixes

--- djspace/registration/forms.py
+++ djspace/registration/forms.py
@@ -3,6 +3,10 @@
 from django import forms
 
 from djspace.registration.models.undergraduate import UndergraduateInformation
+from djspace.registration.models.graduate import GraduateInformation
+from djspace.registration.models.professional import ProfessionalInformation
+from djspace.registration.models.professor import ProfessorInformation
+from djspace.registration.models.k12educator import K12EducatorInformation
 
 class UndergraduateInformationForm(forms.ModelForm):
     """
