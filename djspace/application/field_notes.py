02d18298eb9e91cd20e8d7350017e08735d0243a
Zachary Wenta <zwenta@carthage.edu>
2014-07-28 15:27:49 -0500

    base models

--- djspace/application/field_notes.py
+++ djspace/application/field_notes.py
@@ -47,7 +47,7 @@ K12 Educator (CANNOT TAKE REDUCED GRAVITY FORM)
     """
     student travel
     """
-    purpose of travel
-    location of travel
-    time frame that matches travel (summer 6/1-8/15, summer and fall 6/1-12/10, fall 8/15-12-10, spring 12/10-5/1, summer fall and spring 6/1-5/1, fall and spring 8/15-5/1)
-    file upload (travel proposal)    
+        purpose of travel
+        location of travel
+        time frame that matches travel (summer 6/1-8/15, summer and fall 6/1-12/10, fall 8/15-12-10, spring 12/10-5/1, summer fall and spring 6/1-5/1, fall and spring 8/15-5/1)
+        file upload (travel proposal)    
