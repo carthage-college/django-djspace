941250e9d2e3eaedc3aed362c66fa129f482c731
reZach <reZach@users.noreply.github.com>
2014-09-04 16:14:22 -0500

    Update base_models.py
    added choices for wsgc school

--- djspace/registration/models/base_models.py
+++ djspace/registration/models/base_models.py
@@ -102,6 +102,33 @@ EMPLOYER = (
     ('stillsad','Still sad')
 )
 
+WSGC_SCHOOL = (
+    ('alverno','Alverno College'),
+    ('carroll','Carroll University'),
+    ('carthage','Carthage College'),
+    ('menominee','College of Menominee Nation'),
+    ('lawrence','Lawrence University'),
+    ('marquette','Marquette University'),
+    ('medicalwisconsin','Medical College of Wisconsin'),
+    ('msoe','Milwaukee School of Engineering'),
+    ('ripon','Ripon College'),
+    ('stnorbert','St. Norbert College'),
+    ('uwfoxvalley','UW Fox Valley'),
+    ('uwgreenbay','UW Green Bay'),
+    ('uwlacrosse','UW LaCrosse'),
+    ('uwmadison','UW Madison'),
+    ('uwmilwaukee','UW Milwaukee'),
+    ('uwoshkosh','UW Oshkosh'),
+    ('uwparkside','UW Parkside'),
+    ('uwplatteville','UW Platteville'),
+    ('uwriverfalls','UW River Falls'),
+    ('uwstephenspoint','UW Stevens Point'),
+    ('uwstout','UW Stout'),
+    ('uwsuperior','UW Superior'),
+    ('uwshitewater','UW Whitewater'),
+    ('westerntechnical','Western Technical College'),
+    ('wisconsinlutheran','Wisconsin Lutheran College')
+)
 
 class BasePersonal(models.Model):
 
@@ -241,7 +268,7 @@ class BaseWSGC(models.Model):
     
     wsgc_school = models.CharField(
         "WSGC college enrolled or applied to",
-        max_length=20
+        chocies=WSGC_SCHOOL
     )
     wsgc_school_num = models.CharField(
         "WSGC college enrolled or applied to student number",
