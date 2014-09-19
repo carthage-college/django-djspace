d96c76e7ed7cdd7b20a479c2e58b34b96b3f0842
Bender <plungerman@gmail.com>
2014-09-19 14:28:54 -0500

    forgot to import models

--- djspace/registration/models/professional.py
+++ djspace/registration/models/professional.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
+from django.db import models
 
 from djspace.registration.models.base_models import BasePersonal
+from djspace.registration.choices import WSGC_AFFILIATE
 
 class Professional(BasePersonal):
 
