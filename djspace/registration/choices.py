553b7ceab2f30222ffdcdece93f6dbc3d4973e16
Bender <plungerman@gmail.com>
2014-09-19 14:27:38 -0500

    moved choices tuple to its own file

---
+++ djspace/registration/choices.py
@@ -0,0 +1,76 @@
+YES_NO_DECLINE = (
+    ('Yes', 'Yes'),
+    ('No', 'No'),
+    ('Decline', 'Decline to state')
+)
+
+RACES = (
+    ('American Indian/Alaska Native','American Indian/Alaska Native'),
+    ('Asian','Asian'),
+    ('Black/African American','Black/African American'),
+    ('Caucasian','Caucasian'),
+    ('Hispanic','Hispanic'),
+    ('Native Hawaiian/Other Pacific Islander','Native Hawaiian/Other Pacific Islander'),
+    ('Other','Other')
+)
+
+GRADUATE_DEGREE = (
+    ('M.S.','M.S.'),
+    ('Ph.D','Ph.D'),
+    ('M.D.','M.D.'),
+    ('Other','Other')
+)
+
+UNDERGRADUATE_DEGREE = (
+    ("Bachelor's degree","Bachelor's degree"),
+    ("Associate's degree/certificate","Associate's degree/certificate")
+)
+
+EMPLOYER = (
+    ('sad','OH NO, WE NEED TO FILL THESE OUT'),
+    ('stillsad','Still sad')
+)
+
+WSGC_SCHOOL = (
+    ('alverno','Alverno College'),
+    ('carroll','Carroll University'),
+    ('carthage','Carthage College'),
+    ('menominee','College of Menominee Nation'),
+    ('lawrence','Lawrence University'),
+    ('marquette','Marquette University'),
+    ('medicalwisconsin','Medical College of Wisconsin'),
+    ('msoe','Milwaukee School of Engineering'),
+    ('ripon','Ripon College'),
+    ('stnorbert','St. Norbert College'),
+    ('uwfoxvalley','UW Fox Valley'),
+    ('uwgreenbay','UW Green Bay'),
+    ('uwlacrosse','UW LaCrosse'),
+    ('uwmadison','UW Madison'),
+    ('uwmilwaukee','UW Milwaukee'),
+    ('uwoshkosh','UW Oshkosh'),
+    ('uwparkside','UW Parkside'),
+    ('uwplatteville','UW Platteville'),
+    ('uwriverfalls','UW River Falls'),
+    ('uwstephenspoint','UW Stevens Point'),
+    ('uwstout','UW Stout'),
+    ('uwsuperior','UW Superior'),
+    ('uwshitewater','UW Whitewater'),
+    ('westerntechnical','Western Technical College'),
+    ('wisconsinlutheran','Wisconsin Lutheran College')
+)
+
+WSGC_AFFILIATE = (
+    ('Aerogel Technolgies','Aerogel Technolgies'),
+    ('Astronautics','Astronautics'),
+    ('BTCI','BTCI'),
+    ('Charter','Charter'),
+    ('Crossroads at Big Creek','Crossroads at Big Creek'),
+    ('DOT','DOT'),
+    ('DPI','DPI'),
+    ('EAA','EAA'), 
+    ('Orbitec','Orbitec'),
+    ('Space Explorers','Space Explorers'),
+    ('Space Port','Space Port'),
+    ('Sheboygan', 'Sheboygan'),
+    ('StaatsQuest','StaatsQuest')
+)
