e5fc7177ba9cb339f74a763bde2ebcec53eb1ed2
Bender <plungerman@gmail.com>
2014-11-02 20:07:27 -0600

    new file field validator for file type using python-magic

--- djspace/registration/validators.py
+++ djspace/registration/validators.py
@@ -1,5 +1,21 @@
 from django.core.validators import *
 
+from django.core.exceptions import ValidationError
+import magic
+
+class MimetypeValidator(object):
+    def __init__(self, mimetypes):
+        self.mimetypes = mimetypes
+
+    def __call__(self, value):
+        try:
+            mime = magic.from_buffer(value.read(1024), mime=True)
+            if not mime in self.mimetypes:
+                raise ValidationError('%s is not an acceptable file type' % value)
+        except AttributeError as e:
+            raise ValidationError('This value could not be validated for file type' % value)
+
+
 credit_gpa_validator = RegexValidator(
     regex='^[0-9]{1,}\.[0-9]{1,2}$',
     message='Valid inputs are: eg. 4.00 or 132.5',
