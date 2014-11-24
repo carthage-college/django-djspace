3cfe0bc3115003aa505dbf0217896ad3779f9701
Bender <plungerman@gmail.com>
2014-11-24 11:10:16 -0600

    forgot the generic choice json fixture

--- djspace/bin/cereal.py
+++ djspace/bin/cereal.py
@@ -10,12 +10,12 @@ django.setup()
 
 #with open("profile.json", "w") as out:
 #with open("faculty.json", "w") as out:
-#with open("generic_choice.json", "w") as out:
-with open("tag.json", "w") as out:
+with open("generic_choice.json", "w") as out:
+#with open("tag.json", "w") as out:
 #with open("tagged_item.json", "w") as out:
     json_serializer = serializers.get_serializer('json')()
     #json_serializer.serialize(Faculty.objects.all(), stream=out)
     #json_serializer.serialize(UserProfile.objects.all(), stream=out)
-    #json_serializer.serialize(GenericChoice.objects.all(), stream=out)
-    json_serializer.serialize(Tag.objects.all(), stream=out)
+    json_serializer.serialize(GenericChoice.objects.all(), stream=out)
+    #json_serializer.serialize(Tag.objects.all(), stream=out)
     #json_serializer.serialize(TaggedItem.objects.all(), stream=out)
