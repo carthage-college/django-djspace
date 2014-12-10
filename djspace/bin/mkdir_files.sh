f235f78e8afff938ddf63388793a3465276e2906
Bender <plungerman@gmail.com>
2014-12-10 10:29:21 -0600

    updates to mkdir script to correspond to grant application types and their slug

--- djspace/bin/mkdir_files.sh
+++ djspace/bin/mkdir_files.sh
@@ -2,20 +2,15 @@
 
 cd /d2/django_projects/djspace/static/
 
-mkdir -p files/applications/first-nations-launch-competition/
-
-mkdir -p files/applications/high-altitude-balloon-payload/
-mkdir -p files/applications/high-altitude-balloon-launch/
-
-mkdir -p files/applications/higher-education-initiatives/
-mkdir -p files/applications/research-infrastructure/proposal/
-
-mkdir -p files/applications/undergraduate-scholarship/
-mkdir -p files/applications/undergraduate-research/
-
-mkdir -p files/applications/graduate-fellowship/
 mkdir -p files/applications/clark-graduate-fellowship/
-
+mkdir -p files/applications/first-nations-launch-competition/
+mkdir -p files/applications/graduate-fellowship/
+mkdir -p files/applications/high-altitude-balloon-launch/
+mkdir -p files/applications/high-altitude-balloon-payload/
+mkdir -p files/applications/higher-education-initiatives/
+mkdir -p files/applications/research-infrastructure/
+mkdir -p files/applications/undergraduate-research/
+mkdir -p files/applications/undergraduate-scholarship/
 
 chown -R www-data:staff files
 
