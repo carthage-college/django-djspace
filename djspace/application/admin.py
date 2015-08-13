6186cda39e28952c26bcbd2d367cc236a0c72b89
Bender <plungerman@gmail.com>
2015-08-13 10:08:15 -0500

    used 'less than or equal' rather than just 'less than' when comparing the current month to the grant cycle start month

--- djspace/application/admin.py
+++ djspace/application/admin.py
@@ -23,7 +23,7 @@ def get_queryset(self, request, admin_class):
     YEAR = int(TODAY.year)
     MES = int(TODAY.month)
     qs = super(admin_class, self).queryset(request)
-    if MES < settings.GRANT_CYCLE_START_MES:
+    if MES <= settings.GRANT_CYCLE_START_MES:
         YEAR = YEAR - 1
     start_date = datetime.date(YEAR, settings.GRANT_CYCLE_START_MES, 1)
     return qs.filter(date_created__gte=start_date)
