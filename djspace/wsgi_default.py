fe783d0ecb6ffe06e46e83f1957e5bf041039f6c
Bender <plungerman@gmail.com>
2016-02-14 20:20:37 -0600

    introduced try/except to catch the errors and print to apache2 error log because django1.7+ sometimes throws an error like 'populate() isn't reentrant' which means nothing

--- djspace/wsgi_default.py
+++ djspace/wsgi_default.py
@@ -1,13 +1,9 @@
-"""
-WSGI config for djproj project.
+import os
+import time
+import traceback
+import signal
+import sys
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
-"""
-
-import os, sys
 # python
 sys.path.append('/usr/lib/python2.7/')
 sys.path.append('/usr/lib/python2.7/dist-packages/')
@@ -19,16 +15,19 @@ sys.path.append('/data2/django_third/')
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djspace.settings")
 os.environ.setdefault("PYTHON_EGG_CACHE", "/var/cache/python/.python-eggs")
 os.environ.setdefault("TZ", "America/Chicago")
-# informix
-os.environ['INFORMIXSERVER'] = 'wilson'
-os.environ['DBSERVERNAME'] = 'wilson'
-os.environ['INFORMIXDIR'] = '/opt/ibm/informix'
-os.environ['ODBCINI'] = '/etc/odbc.ini'
-os.environ['ONCONFIG'] = 'onconf.carstrain'
-os.environ['INFORMIXSQLHOSTS'] = '/opt/ibm/informix/etc/sqlhosts'
-os.environ['LD_LIBRARY_PATH'] = '$INFORMIXDIR/lib:$INFORMIXDIR/lib/esql:$INFORMIXDIR/lib/tools:/usr/lib/apache2/modules:$INFORMIXDIR/lib/cli'
-os.environ['LD_RUN_PATH'] = '/opt/ibm/informix/lib:/opt/ibm/informix/lib/esql:/opt/ibm/informix/lib/tools:/usr/lib/apache2/modules'
+
 # wsgi
 from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+
+try:
+    application = get_wsgi_application()
+    #print 'WSGI without exception'
+except Exception:
+    #print 'handling WSGI exception'
+    # Error loading applications
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
+
 
