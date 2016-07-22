490b670089a1a0b205a8946453dba6bae228f05c
plungerman <plungerman@gmail.com>
2016-07-22 10:40:34 -0500

    replace eval() with str_to_class()

--- djspace/dashboard/views.py
+++ djspace/dashboard/views.py
@@ -13,26 +13,34 @@ from djspace.registration.forms import *
 from djspace.core.utils import get_profile_status
 from djspace.dashboard.forms import UserForm, UserProfileForm
 
+from djtools.utils.convert import str_to_class
+
 import json
+
 
 @login_required
 def home(request):
     """
     User dashboard home
     """
-    reg_type = request.user.profile.registration_type
+
+    user = request.user
     try:
-        reg = eval(reg_type).objects.get(user=request.user)
+        mod = str_to_class(
+            "djspace.registration.models",
+            user.profile.registration_type
+        )
+        reg = mod.objects.get(user=user)
     except:
         reg = None
     # if the user does not have any applications, the gm2m
     # field will throw an error
     try:
-        apps = request.user.profile.applications
+        apps = user.profile.applications
     except:
         apps = None
 
-    status = get_profile_status(request.user)
+    status = get_profile_status(user)
     return render_to_response(
         "dashboard/home.html", {"reg":reg,"status":status},
         context_instance=RequestContext(request)
@@ -77,14 +85,16 @@ def registration_type(request):
     if request.method == 'POST':
         reg_type = request.POST.get("registration_type")
         try:
-            reg = eval(reg_type).objects.get(user=request.user)
+            mod = str_to_class(
+                "djspace.registration.models", reg_type
+            )
+            reg = mod.objects.get(user=request.user)
         except:
             reg = None
-
         try:
-            reg_form = eval(reg_type+"Form")(
-                instance=reg, prefix="reg"
-            )
+            reg_form = str_to_class(
+                "djspace.registration.forms", (reg_type+"Form")
+            )(instance=reg, prefix="reg")
         except:
             raise Http404
         reggie = None
@@ -117,13 +127,18 @@ def profile_form(request):
     if request.method == 'POST' and reg_type != request.POST.get("pro-registration_type"):
         reg_type = request.POST.get("pro-registration_type")
     try:
-        reg = eval(reg_type).objects.get(user=user)
+        mod = str_to_class(
+            "djspace.registration.models", reg_type
+        )
+        reg = mod.objects.get(user=user)
     except:
         reg = None
     if request.method == 'POST':
-        reg_form = eval(reg_type+"Form")(
-            instance=reg, prefix="reg", data=request.POST
-        )
+
+        reg_form = str_to_class(
+            "djspace.registration.forms", (reg_type+"Form")
+        )(instance=reg, prefix="reg", data=request.POST)
+
         pro_form = UserProfileForm(
             instance=profile, data=request.POST, prefix="pro"
         )
@@ -149,9 +164,9 @@ def profile_form(request):
             'salutation':profile.salutation,'first_name':user.first_name,
             'second_name':profile.second_name,'last_name':user.last_name
         }, prefix="usr")
-        reg_form = eval(reg_type+"Form")(
-            instance=reg, prefix="reg"
-        )
+        reg_form = str_to_class(
+            "djspace.registration.forms", (reg_type+"Form")
+        )(instance=reg, prefix="reg")
         pro_form = UserProfileForm(instance=profile, prefix="pro")
     return render_to_response(
         "dashboard/profile_form.html", {
