55da3ccade3ac324d2e685c6fd5962c649753ff2
plungerman <plungerman@gmail.com>
2017-03-27 10:06:54 -0500

    django 1.10 changes

--- djspace/application/urls.py
+++ djspace/application/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.views.generic import TemplateView
 from django.contrib.auth.decorators import login_required
 
-urlpatterns = patterns('djspace.application.views',
+from djspace.application import views
+
+urlpatterns = [
     url(
         r'^$',
         login_required(TemplateView.as_view(
@@ -20,18 +22,18 @@ urlpatterns = patterns('djspace.application.views',
     ),
     url(
         r'^(?P<application_type>[a-zA-Z0-9_-]+)/(?P<aid>\d+)/print/$',
-        'application_print', name="application_print"
+        views.application_print, name='application_print'
     ),
     url(
         r'^(?P<application_type>[a-zA-Z0-9_-]+)/(?P<aid>\d+)/update/$',
-        'application_form', name="application_update"
+        views.application_form, name='application_update'
     ),
     url(
         r'^(?P<application_type>[a-zA-Z0-9_-]+)/export/$',
-        'application_export', name="application_export"
+        views.application_export, name='application_export'
     ),
     url(
         r'^(?P<application_type>[a-zA-Z0-9_-]+)/$',
-        'application_form', name="application_create"
+        views.application_form, name='application_create'
     ),
-)
+]
