632ef39f023854e0037ba50a9c5c5c105933a5d9
plungerman <plungerman@gmail.com>
2017-05-02 13:08:11 -0500

    going to production

--- djspace/bin/gm2m.py
+++ djspace/bin/gm2m.py
@@ -1,25 +1,38 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+# env
+sys.path.append('/usr/lib/python2.7/dist-packages/')
+sys.path.append('/usr/lib/python2.7/')
+sys.path.append('/usr/local/lib/python2.7/dist-packages/')
+sys.path.append('/data2/django_1.11/')
+sys.path.append('/data2/django_projects/')
+sys.path.append('/data2/django_third/')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djspace.settings')
+
+
 import django
 
+from django.conf import settings
 django.setup()
 
 from django.contrib.auth.models import User
 
-users = User.objects.all().order_by("last_name")
-program = None
-exports = []
+#users = User.objects.all().order_by('last_name')
+user = User.objects.get(pk=1)
 
-for user in users:
-    try:
-        apps = user.profile.applications.all()
-    except:
-        apps = None
+apps = user.profile.applications.all()
 
-    if apps:
-        for a in apps:
-            #print a
-            print a.__dict__
-            #print a._state.__dict__
-            #print a.id
-            # don't work
-            #print a.all()
-            #print a.get_related_models()
+print apps
+'''
+if apps:
+    for a in apps:
+        #print a
+        print a.__dict__
+        #print a._state.__dict__
+        #print a.id
+        # don't work
+        #print a.all()
+        #print a.get_related_models()
+'''
