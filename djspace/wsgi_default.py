19a92239db8ae9e9958ac21fba46a7ab91979a8d
plungerman <plungerman@gmail.com>
2020-11-06 13:56:18 -0600

    removed debug setup and updated settings path

--- djspace/wsgi_default.py
+++ djspace/wsgi_default.py
@@ -1,7 +1,8 @@
+# -*- coding: utf-8 -*-
+
+"""WSGI configuration."""
+
 import os
-import time
-import traceback
-import signal
 import sys
 
 # python
@@ -10,20 +11,9 @@ sys.path.append('/data2/python_venv/2.7/djspace/lib/python2.7/site-packages/')
 sys.path.append('/data2/python_venv/2.7/djspace/lib/django_projects/')
 sys.path.append('/data2/python_venv/2.7/djspace/lib/django-djspace/')
 # django
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djspace.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djspace.settings.production')
 os.environ.setdefault('PYTHON_EGG_CACHE', '')
 os.environ.setdefault('TZ', 'America/Chicago')
 # wsgi
 from django.core.wsgi import get_wsgi_application
-
-# NOTE: remove the try/except in production
-#application = get_wsgi_application()
-try:
-    application = get_wsgi_application()
-except Exception:
-    # Error loading applications
-    if 'mod_wsgi' in sys.modules:
-        traceback.print_exc()
-        os.kill(os.getpid(), signal.SIGINT)
-        time.sleep(2.5)
-    exit(-1)
+application = get_wsgi_application()
