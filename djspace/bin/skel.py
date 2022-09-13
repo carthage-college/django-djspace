bfedaec81cca21f02cb665a32dacdb42b21f7207
plungerman <plungerman@gmail.com>
2022-09-13 15:07:23 -0500

    updated shell skeleton file from djskeletor

--- djspace/bin/skel.py
+++ djspace/bin/skel.py
@@ -1,52 +1,63 @@
+#! /usr/bin/env python3
 # -*- coding: utf-8 -*-
-import os, sys
+
+import os
+import sys
 
 # env
-sys.path.append('/usr/lib/python2.7/dist-packages/')
-sys.path.append('/usr/lib/python2.7/')
-sys.path.append('/usr/local/lib/python2.7/dist-packages/')
-sys.path.append('/data2/django_projects/')
-sys.path.append('/data2/django_third/')
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "djspace.settings")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djspace.settings.shell')
+
+# required if using django models
+import django
+django.setup()
 
 from django.conf import settings
 
-from optparse import OptionParser
+import argparse
+import logging
 
-"""
-Shell script...
-"""
+logger = logging.getLogger('debug_logfile')
 
 # set up command-line options
 desc = """
 Accepts as input...
 """
 
-parser = OptionParser(description=desc)
-parser.add_option(
-    "-x", "--equis",
-    help="Lorem ipsum dolor sit amet.",
-    dest="equis"
+# RawTextHelpFormatter method allows for new lines in help text
+parser = argparse.ArgumentParser(
+    description=desc, formatter_class=argparse.RawTextHelpFormatter,
 )
 
+parser.add_argument(
+    '-x',
+    '--equis',
+    required=True,
+    help="Lorem ipsum dolor sit amet.",
+    dest='equis',
+)
+parser.add_argument(
+    '--test',
+    action='store_true',
+    help="Dry run?",
+    dest='test',
+)
+
+
 def main():
-    """
-    main method
-    """
+    """Main function description."""
+    if test:
+        print("this is a test")
+        logger.debug("debug = %s" % test)
+    else:
+        print("this is not a test")
 
-######################
-# shell command line
-######################
 
-if __name__ == "__main__":
-    (options, args) = parser.parse_args()
-    equis = options.equis
+if __name__ == '__main__':
+    args = parser.parse_args()
+    equis = args.equis
+    test = args.test
 
-    mandatories = ['equis',]
-    for m in mandatories:
-        if not options.__dict__[m]:
-            print "mandatory option is missing: %s\n" % m
-            parser.print_help()
-            exit(-1)
+    if test:
+        print(args)
 
     sys.exit(main())
