7b10310aa5f045c41d540133a91d5307a2ef686e
plungerman <plungerman@gmail.com>
2023-04-25 09:28:21 -0500

    updates to wsgi infrastructure

---
+++ djspace/wsgi.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""WSGI configuration."""
+
+import os
+import sys
+
+from django.core.wsgi import get_wsgi_application
+
+
+# python
+sys.path.append('/data2/python_venv/3.8/djspace/lib/python3.8/')
+sys.path.append('/data2/python_venv/3.8/djspace/lib/python3.8/site-packages/')
+# django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djspace.settings.shell')
+os.environ.setdefault('PYTHON_EGG_CACHE', '/var/cache/python/.python-eggs')
+os.environ.setdefault('TZ', 'America/Chicago')
+# wsgi
+application = get_wsgi_application()
